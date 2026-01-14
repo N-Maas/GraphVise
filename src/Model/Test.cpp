c53115a12e6bb8175d64cf2d38abe6164f2b6efd
jannis <uhngk@student.kit.edu>
2026-01-14 21:39:41 +0100

    Add Model package + Test Class

---
+++ src/Model/Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by janni on 1/14/26.
+//
+
+#include "Test.hpp"
---
+++ src/Model/Test.hpp
@@ -0,0 +1,14 @@
+//
+// Created by janni on 1/14/26.
+//
+
+#ifndef THESIS_FRAMEWORK_TEST_HPP
+#define THESIS_FRAMEWORK_TEST_HPP
+
+
+class Test
+{
+};
+
+
+#endif //THESIS_FRAMEWORK_TEST_HPP
