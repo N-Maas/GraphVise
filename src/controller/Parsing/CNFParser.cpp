c0b4d8c04dcbbdbce3ea0f6de7b0f2f71aeae395
TimMcCool <timmccool.scratch@gmail.com>
2026-01-19 08:48:15 +0100

    Added CNF Parser classes

---
+++ src/controller/Parsing/CNFParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tim on 19.01.26.
+//
+
+#include "CNFParser.hpp"
---
+++ src/controller/Parsing/CNFParser.hpp
@@ -0,0 +1,19 @@
+//
+// Created by tim on 19.01.26.
+//
+
+#ifndef THESIS_FRAMEWORK_CNFPARSER_HPP
+#define THESIS_FRAMEWORK_CNFPARSER_HPP
+#include <string>
+
+#include "../Structs/GraphData.hpp"
+
+#endif //THESIS_FRAMEWORK_CNFPARSER_HPP
+
+class CNFParser {
+    public:
+    //Destruktor
+    virtual ~GraphParser() = default;
+
+    virtual GraphData parseFile(std::string filePath) = 0;
+};
