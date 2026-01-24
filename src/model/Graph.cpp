2dfe556b3077b22a268464a2653046fb243c9720
jannis <uhngk@student.kit.edu>
2026-01-24 14:36:58 +0100

    corrected deleteAllGroups (added Default Group after deleting + all Edges/Vertices have Group 0 after deleting)

--- src/model/Graph.cpp
+++ src/model/Graph.cpp
@@ -96,6 +96,13 @@ void Graph::removeAllHighlights() const {
 
 void Graph::deleteAllGroups() {
     groups.clear();
+    addGroup("Default-Group", ImVec4{0,134,139,1}, std::vector<std::uint32_t>{}, std::vector<std::uint32_t>{});
+    for (Vertex vertex : vertices) {
+        vertex.setGroup(0);
+    }
+    for (Edge edge : edges) {
+        edge.setGroup(0);
+    }
 }
 
 void Graph::deleteCameraBookmarks(const std::uint32_t cameraBookmarkID) {
