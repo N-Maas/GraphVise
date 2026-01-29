08733069710971bf9bfb9738c282061e9d9d0327
Yannik <y.tobian@gmail.com>
2026-01-29 16:31:34 +0100

    Fixed return for errors

--- src/controller/ButtonController.cpp
+++ src/controller/ButtonController.cpp
@@ -61,12 +61,18 @@ namespace graphvise {
         //TODO: Make camera jump to edge?
         Graph& graph = GraphSaver::getGraphSaver().getGraph();
         uint32_t maxVertexID = graph.getVertices().size() - 1;
+
         if (firstVertexID == secondVertexID) {
             ErrorCollector::getInstance().collectError(Error(ErrorType::EQUAL_VERTEX_IDS));
-        } else if (firstVertexID > maxVertexID) {
+            return;
+        }
+        if (firstVertexID > maxVertexID) {
             ErrorCollector::getInstance().collectError(Error(ErrorType::VERTEX_ID_OUT_OF_BOUNDS, std::to_string(firstVertexID)));
-        } else if (secondVertexID > maxVertexID) {
+            return;
+        }
+        if (secondVertexID > maxVertexID) {
             ErrorCollector::getInstance().collectError(Error(ErrorType::VERTEX_ID_OUT_OF_BOUNDS, std::to_string(secondVertexID)));
+            return;
         }
 
         uint32_t edgeID = graph.getEdgeIDByConnectingVerticesIDs(firstVertexID, secondVertexID);
