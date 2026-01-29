b45d320a725bcb781b61a36ea8b28a67755c03a8
Yannik <y.tobian@gmail.com>
2026-01-29 15:27:10 +0100

    Fixed WEmbedController, so it creates a wembed graph from neighborhoods instead of an edge list

--- src/controller/Parsing/WEmbedController.cpp
+++ src/controller/Parsing/WEmbedController.cpp
@@ -4,11 +4,13 @@
 
 #include "WEmbedController.hpp"
 
+#include "AdamOptimizer.hpp"
 #include "wembed.h"
 
 namespace graphvise {
     Graph WEmbedController::embedGraph(GraphData& graphData) {
-        wembed::Graph graph = wembed::graphFromEdges(graphData.edges);
+        std::map<int, std::set<int>> neighborhoodMap = createNeighborhoodMap(graphData);
+        wembed::Graph graph = wembed::graphFromNeighborhoods(neighborhoodMap);
         wembed::Options embedderOptions;
         embedderOptions.embeddingDimension = 3;
         embedderOptions.useWeights = false;
@@ -29,4 +31,19 @@ namespace graphvise {
 
         return embeddedGraph;
     }
+
+    std::map<int, std::set<int>> WEmbedController::createNeighborhoodMap(GraphData& graphData) {
+        std::map<int, std::set<int>> neighbors;
+        for (int i = 0; i < graphData.vertexCount; i++) {
+            neighbors[i] = std::set<int>();
+        }
+
+        for (auto edge : graphData.edges) {
+            neighbors[edge.first].insert(edge.second);
+            neighbors[edge.second].insert(edge.first);
+        }
+
+        return neighbors;
+    }
+
 }
--- src/controller/Parsing/WEmbedController.hpp
+++ src/controller/Parsing/WEmbedController.hpp
@@ -4,6 +4,9 @@
 
 #ifndef THESIS_FRAMEWORK_WEMBEDCONTROLLER_HPP
 #define THESIS_FRAMEWORK_WEMBEDCONTROLLER_HPP
+#include <map>
+#include <set>
+
 #include "model/Graph.hpp"
 #include "controller/Structs/GraphData.hpp"
 
@@ -11,6 +14,9 @@ namespace graphvise {
     class WEmbedController {
     public:
         Graph embedGraph(GraphData& graphData);
+
+    private:
+        std::map<int, std::set<int>> createNeighborhoodMap(GraphData& graphData);
     };
 }
 #endif //THESIS_FRAMEWORK_WEMBEDCONTROLLER_HPP
