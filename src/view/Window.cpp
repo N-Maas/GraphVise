7ffa2b1a56545df975acdcf924154899d29cc7c7
unjeg <unjeg@student.kit.edu>
2026-02-17 19:23:07 +0100

    removed double runFrame call in Window

--- src/view/Window.cpp
+++ src/view/Window.cpp
@@ -194,10 +194,6 @@ namespace graphvise {
             renderer->runFrame();
             GL_CHECK_ERROR();
 
-
-            renderer->runFrame();
-
-
             //load GUI
             gui->loadFrame(framebufferWidth, framebufferHeight);
 
