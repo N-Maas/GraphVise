f920da812036b87222215f588095bd68a8c9ed77
jannis <uhngk@student.kit.edu>
2026-02-17 20:42:02 +0100

    adjusted the Test Cases

--- tests/tests_Vertex.cpp
+++ tests/tests_Vertex.cpp
@@ -15,26 +15,27 @@ TEST(VertexTest, CheckGetter) {
     EXPECT_EQ(vertex.getCoordsVector().x, 1.0f);
     EXPECT_EQ(vertex.getCoordsVector().y, 3.0f);
     EXPECT_EQ(vertex.getCoordsVector().z, 2.5f);
-    EXPECT_NEAR(vertex.getVec4().x, 1, 0.0001);    //Value of the Default-Group
-    EXPECT_NEAR(vertex.getVec4().y, 0, 0.0001);  //Value of the Default-Group
-    EXPECT_NEAR(vertex.getVec4().z, 0, 0.0001);  //Value of the Default-Group
-    EXPECT_EQ(vertex.getVec4().w, 1);    //Value of the Default-Group
+    EXPECT_NEAR(graph.getVertexVec4ByID(vertex.getID()).x, 1, 0.0001);    //Value of the Default-Group
+    EXPECT_NEAR(graph.getVertexVec4ByID(vertex.getID()).y, 0, 0.0001);  //Value of the Default-Group
+    EXPECT_NEAR(graph.getVertexVec4ByID(vertex.getID()).z, 0, 0.0001);  //Value of the Default-Group
+    EXPECT_EQ(graph.getVertexVec4ByID(vertex.getID()).w, 1);    //Value of the Default-Group
 }
 
 TEST(VertexTest, TransparencyTest) {
     Graph graph = Graph(std::vector{glm::vec3(1.0f, 3.0f, 2.5f)}, std::vector{std::pair<std::uint32_t, std::uint32_t>{3,4}});
     GraphSaver::getInstance().setGraph(graph);
-    Vertex vertex = graph.getVertexByID(0);
+    Graph graph1 = GraphSaver::getInstance().getGraph();
+    Vertex vertex = graph1.getVertexByID(0);
 
-    EXPECT_EQ(vertex.getVec4().w, 1.0f);   //Value of the Default-Group
-    vertex.setOwnTransparency(0.6f);
+    EXPECT_EQ(graph1.getVertexVec4ByID(vertex.getID()).w, 1.0f);   //Value of the Default-Group
+    graph1.getVertexByID(0).setOwnTransparency(0.6f);
     EXPECT_EQ(GraphSaver::getInstance().getGraph().getGroupByID(0).getVec4().w, 1);
-    EXPECT_EQ(vertex.getVec4().w, 0.6f);
-    vertex.deleteOwnTransparency();
-    EXPECT_EQ(vertex.getVec4().w, 1.0f);   //Value of the Default-Group
+    EXPECT_EQ(graph1.getVertexVec4ByID(vertex.getID()).w, 0.6f);
+    graph1.getVertexByID(0).deleteOwnTransparency();
+    EXPECT_EQ(graph1.getVertexVec4ByID(vertex.getID()).w, 1.0f);   //Value of the Default-Group
     EXPECT_EQ(GraphSaver::getInstance().getGraph().getGroupByID(0).getVec4().w, 1);
     EXPECT_THROW(vertex.setOwnTransparency(4.7f), std::out_of_range);
-    EXPECT_EQ(vertex.getVec4().w, 1.0f);
+    EXPECT_EQ(graph1.getVertexVec4ByID(vertex.getID()).w, 1.0f);
 }
 
 TEST(VertexTest, SetGroupTest) {
