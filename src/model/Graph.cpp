d9ecbe22f8c2940d0a8be223c873313ebf57a6d2
Yannik Tobian <y.tobian@gmail.com>
2026-03-09 13:34:27 +0100

    Updated scaling

--- src/model/Graph.cpp
+++ src/model/Graph.cpp
@@ -149,6 +149,9 @@ namespace graphvise {
             newCoords.z = (newScale / manualScale) * vertex.getCoordsVector().z;
             vertex.setCoordsVector(newCoords);
         }
+        for (auto edge : edges) {
+            initRenderingMatrixForEdge(edge);
+        }
         manualScale = newScale;
     }
 
