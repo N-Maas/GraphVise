b302d11b0fc3e8036162a7a6ae501ff54c32439d
Emile Brückner <emile.brueckner@gmail.com>
2026-03-10 10:32:41 +0100

    Adjusted camera movement to accept WASD and up/down keys for center of mass mode.

--- src/view/InputManager.cpp
+++ src/view/InputManager.cpp
@@ -36,20 +36,25 @@ namespace graphvise
         glfwPollEvents();
 
 
-        bool sprinting = (glfwGetKey(window, sprintKey) == GLFW_PRESS);
+        const bool sprinting = (glfwGetKey(window, sprintKey) == GLFW_PRESS);
 
 
         if (!ImGui::GetIO().WantCaptureKeyboard)
         {
             //Moving Camera
             glm::vec3 direction(0, 0, 0);
+
             direction.z += (glfwGetKey(window, frontKey) == GLFW_PRESS) ? 1.0f : 0.0f;
             direction.z -= (glfwGetKey(window, backKey) == GLFW_PRESS) ? 1.0f : 0.0f;
+
             direction.x += (glfwGetKey(window, rightKey) == GLFW_PRESS) ? 1.0f : 0.0f;
             direction.x -= (glfwGetKey(window, leftKey) == GLFW_PRESS) ? 1.0f : 0.0f;
+
             direction.y += (glfwGetKey(window, upKey) == GLFW_PRESS) ? 1.0f : 0.0f;
             direction.y -= (glfwGetKey(window, downKey) == GLFW_PRESS) ? 1.0f : 0.0f;
+
             movementController.moveCamera(direction, sprinting);
+
         }
 
         //Rotating Camera
@@ -82,7 +87,7 @@ namespace graphvise
 
         if (!ImGui::GetIO().WantCaptureMouse)
         {
-            movementController.zoom(-scrollYOffset, sprinting);
+            movementController.zoom(- scrollYOffset, sprinting);
             scrollYOffset = 0;
         }
         handleMouseClick();
@@ -121,7 +126,6 @@ namespace graphvise
         scrollYOffset = yoffset;
     }
 
-    // Implementation in Window.cpp:
     void InputManager::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
     {
         mouseButtonPressed = true;
@@ -140,7 +144,7 @@ namespace graphvise
 
         if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
         {
-            auto renderer = Renderer::getInstance();
+            const auto renderer = Renderer::getInstance();
 
             double xpos, ypos;
 
